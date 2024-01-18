f25fede61a1977d0c82060149306a631cd38c55d
Cameron Weijmer <weijmer.c@gmail.com>
2024-01-18 13:33:45 +0100

    feat: added Usersettings

--- app/src/main/java/com/example/walkr/UserSettings.java
+++ app/src/main/java/com/example/walkr/UserSettings.java
@@ -1,4 +1,29 @@
 package com.example.walkr;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class UserSettings {
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class UserSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usersettings);
+
+        EditText nameEditText = findViewById(R.id.nameEditText);
+        EditText stepgoalEditText = findViewById(R.id.stepgoalEditText);
+        Button saveButton = findViewById(R.id.saveButton);
+
+        saveButton.setOnClickListener(view -> {
+            Intent resultIntent = new Intent(this, MainActivity.class);
+            resultIntent.putExtra(MainActivity.NAME_INTENT_PARAM, nameEditText.getText().toString());
+            resultIntent.putExtra(MainActivity.STEPGOAL_INTENT_PARAM, Double.parseDouble(stepgoalEditText.getText().toString()));
+            startActivity(resultIntent);
+        });
+    }
+
 }
