5526e924b3d16e5a1e10652a9bd7d2032465a121
Cameron Weijmer <weijmer.c@gmail.com>
2024-01-18 15:39:27 +0100

    fix: added View parameter to onclick event method

--- app/src/main/java/com/example/walkr/UserSettings.java
+++ app/src/main/java/com/example/walkr/UserSettings.java
@@ -2,6 +2,7 @@ package com.example.walkr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,7 +18,7 @@ public class UserSettings extends AppCompatActivity {
 
     }
 
-    public void onSaveButtonClick() {
+    public void onSaveButtonClick(View v) {
         EditText nameEditText = findViewById(R.id.nameEditText);
         EditText stepgoalEditText = findViewById(R.id.stepgoalEditText);
         Intent resultIntent = new Intent(this, MainActivity.class);
